efa999b7c59929bb98a72c12a69391ead6143088
razsela <razisela12@gmail.com>
2023-10-06 18:39:49 +0300

    l

--- Projects/Memory_game.js
+++ Projects/Memory_game.js
@@ -1,4 +1,5 @@
-const cards = document.querySelectorAll('.tile');
+const cards4 = document.querySelectorAll('.tile1');
+const cards6 = document.querySelectorAll('.tile2');
 
 let clicks = 0;
 let hasFlippedCard = false;
@@ -71,17 +72,24 @@ function unflipCards() {
 }
 
 function shuffle() {
-    cards.forEach(card => {
+    cards4.forEach(card => {
         let randomPos = Math.floor(Math.random() * 16);
         card.style.order = randomPos;
     });
-    cards.forEach(card => card.addEventListener('click', flipCard));
+    cards4.forEach(card => card.addEventListener('click', flipCard));
+    cards6.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 16);
+        card.style.order = randomPos;
+    });
+    cards6.forEach(card => card.addEventListener('click', flipCard));
 }
 
 
 function reset_btn() {
 
-    cards.forEach(card => card.classList.remove('flip'));
+    cards4.forEach(card => card.classList.remove('flip'));
+    cards6.forEach(card => card.classList.remove('flip'));
+
     setTimeout(() => {
         resetBoard();
         shuffle();
@@ -97,7 +105,8 @@ function updateScores() {
     document.getElementById('clicks').innerText = clicks;
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+cards4.forEach(card => card.addEventListener('click', flipCard));
+cards6.forEach(card => card.addEventListener('click', flipCard));
 
 shuffle();
 
@@ -105,13 +114,19 @@ shuffle();
 const menuButton = document.getElementById('menu');
 menuButton.addEventListener('click', () => {
 
-    document.getElementById('pattern1').style.display = 'none';
+    document.getElementById('pattern4').style.display = 'none';
+    document.getElementById('pattern6').style.display = 'none';
     document.getElementById('counter').style.display = 'none';
+    document.getElementById('spongebob_btn_easy').style.display = 'none';
+    document.getElementById('spongebob_btn_hard').style.display = 'none';
     document.getElementById('spongebob_btn').style.display = '';
     document.getElementById('reset_btn').style.display = 'none';
     document.getElementById('shoes_btn').style.display = '';
+    document.getElementById('shoes_btn_hard').style.display = 'none';
+    document.getElementById('shoes_btn_easy').style.display = 'none';
     document.getElementById('cars_btn').style.display = '';
     document.getElementById('select_subject').style.display = '';
+    document.getElementById('select_level').style.display = 'none';
     document.getElementById("background").style.backgroundImage = "url(images/memory_game_background.png)";
 
 
@@ -149,6 +164,8 @@ function show_spongebob4() {
     var subject = document.getElementById('select_subject');
     var spongebob_hard = document.getElementById('spongebob_btn_hard');
     var spongebob_easy = document.getElementById('spongebob_btn_easy');
+    var select_level = document.getElementById('select_level');
+    select_level.style.display = 'none';
     spongebob_easy.style.display = 'none';
     spongebob_hard.style.display = 'none';
     subject.style.display = 'none';
@@ -180,9 +197,9 @@ function show_spongebob4() {
     
     ];
 
-    const tiles = document.querySelectorAll('.tile');
+    const tiles = document.querySelectorAll('.tile1');
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < tiles.length; i++) {
         const frontFaceImg = tiles[i].querySelector('.front-face');
         const backFaceImg = tiles[i].querySelector('.back-face');
         frontFaceImg.src = spongebob_images[i];
@@ -201,6 +218,8 @@ function show_spongebob6() {
     var subject = document.getElementById('select_subject');
     var spongebob_hard = document.getElementById('spongebob_btn_hard');
     var spongebob_easy = document.getElementById('spongebob_btn_easy');
+    var select_level = document.getElementById('select_level');
+    select_level.style.display = 'none';
     spongebob_easy.style.display = 'none';
     spongebob_hard.style.display = 'none';
     subject.style.display = 'none';
@@ -231,7 +250,7 @@ function show_spongebob6() {
         'images/magic_boy.png',
         'images/sea_man.png',
         'images/ray.png',
-        
+
         'images/spongebob_character.png',
         'images/squidward.png',
         'images/sandy.png',
@@ -251,7 +270,7 @@ function show_spongebob6() {
         'images/squilliam.png',
         'images/ray.png'
     ];
-    const tiles = document.querySelectorAll('.tile');
+    const tiles = document.querySelectorAll('.tile2');
 
     for (let i = 0; i < tiles.length; i++) {
         const frontFaceImg = tiles[i].querySelector('.front-face');
@@ -263,15 +282,39 @@ function show_spongebob6() {
     }
 }
 
+function shoes_level() {
+    var reset_btn1 = document.getElementById('reset_btn');
+    var counter = document.getElementById('counter');
+    var shoes_btn = document.getElementById('shoes_btn');
+    var spongebob_btn = document.getElementById('spongebob_btn');
+    var cars_btn = document.getElementById('cars_btn');
+    var subject = document.getElementById('select_subject');
+    var shoes_hard = document.getElementById('shoes_btn_hard');
+    var shoes_easy = document.getElementById('shoes_btn_easy');
+    var select_level = document.getElementById('select_level');
+    select_level.style.display = '';
+    shoes_easy.style.display = '';
+    shoes_hard.style.display = '';
+    subject.style.display = 'none';
+    cars_btn.style.display = 'none';
+    shoes_btn.style.display = 'none';
+    spongebob_btn.style.display = 'none';
 
-function show_shoes() {
-    var board = document.getElementById('pattern1');
+}
+function show_shoes4() {
+    var board = document.getElementById('pattern4');
     var reset_btn1 = document.getElementById('reset_btn');
     var counter = document.getElementById('counter');
     var shoes_btn = document.getElementById('shoes_btn');
     var play_btn = document.getElementById('spongebob_btn');
     var cars_btn = document.getElementById('cars_btn');
     var subject = document.getElementById('select_subject');
+    var shoes_hard = document.getElementById('shoes_btn_hard');
+    var shoes_easy = document.getElementById('shoes_btn_easy');
+    var select_level = document.getElementById('select_level');
+    select_level.style.display = 'none';
+    shoes_easy.style.display = 'none';
+    shoes_hard.style.display = 'none';
     subject.style.display = 'none';
     cars_btn.style.display = 'none';
     shoes_btn.style.display = 'none';
@@ -301,20 +344,73 @@ function show_shoes() {
         'images/shoes_pair_8.png',
     ];
 
-    const tiles = document.querySelectorAll('.tile');
+    const tiles = document.querySelectorAll('.tile1');
 
     for (let i = 0; i < tiles.length; i++) {
         const frontFaceImg = tiles[i].querySelector('.front-face');
         const backFaceImg = tiles[i].querySelector('.back-face');
         frontFaceImg.src = shoe_images[i];
 
-        // you can change it to a shoe image if you want, instead of bob spong
         backFaceImg.src = 'images/shoes_backface.png';
     }
 }
 
-function show_cars() {
-    var board = document.getElementById('pattern1');
+function show_shoes6() {
+    var board = document.getElementById('pattern6');
+    var reset_btn1 = document.getElementById('reset_btn');
+    var counter = document.getElementById('counter');
+    var shoes_btn = document.getElementById('shoes_btn');
+    var play_btn = document.getElementById('spongebob_btn');
+    var cars_btn = document.getElementById('cars_btn');
+    var subject = document.getElementById('select_subject');
+    var shoes_hard = document.getElementById('shoes_btn_hard');
+    var shoes_easy = document.getElementById('shoes_btn_easy');
+    var select_level = document.getElementById('select_level');
+    select_level.style.display = 'none';
+    shoes_easy.style.display = 'none';
+    shoes_hard.style.display = 'none';
+    subject.style.display = 'none';
+    cars_btn.style.display = 'none';
+    shoes_btn.style.display = 'none';
+    play_btn.style.display = 'none';
+    board.style.display = 'grid';
+    reset_btn1.style.display = '';
+    counter.style.display = '';
+    document.getElementById("background").style.backgroundImage = "url(images/shoes_background.png)";    // Update image sources for shoes
+
+    // Update image sources for shoes
+    const shoe_images = [
+        'images/shoes_pair_1.png',
+        'images/shoes_pair_2.png',
+        'images/shoes_pair_3.png',
+        'images/shoes_pair_4.png',
+        'images/shoes_pair_5.png',
+        'images/shoes_pair_6.png',
+        'images/shoes_pair_7.png',
+        'images/shoes_pair_8.png',
+        'images/shoes_pair_1.png',
+        'images/shoes_pair_2.png',
+        'images/shoes_pair_3.png',
+        'images/shoes_pair_4.png',
+        'images/shoes_pair_5.png',
+        'images/shoes_pair_6.png',
+        'images/shoes_pair_7.png',
+        'images/shoes_pair_8.png',
+    ];
+
+    const tiles = document.querySelectorAll('.tile2');
+
+    for (let i = 0; i < tiles.length; i++) {
+        const frontFaceImg = tiles[i].querySelector('.front-face');
+        const backFaceImg = tiles[i].querySelector('.back-face');
+        frontFaceImg.src = shoe_images[i];
+
+        backFaceImg.src = 'images/shoes_backface.png';
+    }
+}
+
+function show_cars4() {
+    var board = document.getElementById('pattern4');
     var reset_btn1 = document.getElementById('reset_btn');
     var counter = document.getElementById('counter');
     var shoes_btn = document.getElementById('shoes_btn');
@@ -350,7 +446,7 @@ function show_cars() {
         'images/wolswagen.png'
     ];
 
-    const tiles = document.querySelectorAll('.tile');
+    const tiles = document.querySelectorAll('.tile1');
 
     for (let i = 0; i < tiles.length; i++) {
         const frontFaceImg = tiles[i].querySelector('.front-face');
